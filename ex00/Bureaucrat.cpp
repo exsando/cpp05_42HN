602ce936201b5a9aedfff4dc946f23a6808dea72
asando <alexando17@yahoo.com>
2026-09-04 20:31:50 +0200

    WIP: ex00

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,24 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:47:48 by asando            #+#    #+#             */
-/*   Updated: 2026/09/04 16:47:50 by asando           ###   ########.fr       */
+/*   Updated: 2026/09/04 20:31:40 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+#include <string>
+
+Bureaucrat::Bureaucrat(std::string& name, int grade)
+	: _name(name), _grade(grade) {
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+	: _name(other._name), _grade(other._grade) {}
+
+Bureaucrat::~Bureaucrat(){}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& rhs) {
+	if (this != &rhs)
+		_grade = rhs._grade;
+	return *this;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,8 +6,29 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:47:15 by asando            #+#    #+#             */
-/*   Updated: 2026/09/04 16:47:19 by asando           ###   ########.fr       */
+/*   Updated: 2026/09/04 20:31:39 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
 
+# include <string>
+
+class	Bureaucrat {
+	private:
+		const std::string	_name;
+		int					_grade;
+
+	public:
+		Bureaucrat(std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat&	operator=(const Bureaucrat& rhs);
+		~Bureaucrat();
+
+		const std::string	getName(void) const;
+		int					getGrade(void) const;
+		void				setIncGrade(void);
+		void				setDecGrade(void);
+};
+#endif
