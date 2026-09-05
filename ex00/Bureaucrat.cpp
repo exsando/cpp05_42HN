866470757b8ca4133318f1a197ba3ca4408cb9eb
asando <alexando17@yahoo.com>
2026-09-05 15:11:37 +0200

    ADD: Function definition in Bureaucrat.cpp

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,15 +6,21 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:47:48 by asando            #+#    #+#             */
-/*   Updated: 2026/09/04 20:31:40 by asando           ###   ########.fr       */
+/*   Updated: 2026/09/05 15:11:15 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include <string>
+#include <iostream>
 
 Bureaucrat::Bureaucrat(std::string& name, int grade)
 	: _name(name), _grade(grade) {
+		if (_grade < 1)
+			throw GradeTooHighExcep();
+		if (_grade > 150)
+			throw GradeTooLowExcep();
+		return ;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other)
@@ -26,4 +32,37 @@ Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& rhs) {
 	if (this != &rhs)
 		_grade = rhs._grade;
 	return *this;
+}
+
+const std::string& Bureaucrat::getName(void) const {
+	return _name;
+}
+
+int	Bureaucrat::getGrade(void) const {
+	return _grade;
+}
+
+void	Bureaucrat::setIncGrade(void) {
+	if (_grade - 1 < 1)
+		throw GradeTooHighExcep();
+	_grade--;
+}
+
+void	Bureaucrat::setDecGrade(void) {
+	if (_grade + 1 > 150)
+		throw GradeTooLowExcep();
+	_grade++;
+}
+
+const char*	Bureaucrat::GradeTooHighExcep::what() const throw() {
+	return "Grade too High!";
+}
+
+const char*	Bureaucrat::GradeTooLowExcep::what() const throw() {
+	return "Grade too Low!";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b) {
+	os << b.getName() << ", bureaucrat grade " << b.getGrade();
+	return os;
 }
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:47:15 by asando            #+#    #+#             */
-/*   Updated: 2026/09/04 20:31:39 by asando           ###   ########.fr       */
+/*   Updated: 2026/09/05 15:11:18 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,21 @@ class	Bureaucrat {
 		Bureaucrat&	operator=(const Bureaucrat& rhs);
 		~Bureaucrat();
 
-		const std::string	getName(void) const;
+		const std::string&	getName(void) const;
 		int					getGrade(void) const;
 		void				setIncGrade(void);
 		void				setDecGrade(void);
+
+		class GradeTooHighExcep : std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowExcep : std::exception {
+			public:
+				const char* what() const throw();
+		};
 };
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 #endif
