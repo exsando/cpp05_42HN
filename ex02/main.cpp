7865a1683befbd1769db4980ef1431447f8f977b
asando <alexando17@yahoo.com>
2026-09-20 07:45:04 +0200

    FIX

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,20 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 11:17:02 by asando            #+#    #+#             */
-/*   Updated: 2026/09/13 11:17:04 by asando           ###   ########.fr       */
+/*   Updated: 2026/09/19 20:34:59 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+int	main(void) {
+	Bureaucrat bu("Test", 50);
+	ShrubberyCreationForm testForm("home");
+	bu.signForm(testForm);
+	bu.executeForm(testForm);
+	return (0);
+}
